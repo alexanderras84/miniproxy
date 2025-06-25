09b711889912e87fa88266268946d72218e80198
alexanderras84 <alexanderras@gmail.com>
2025-06-25 19:57:25 +0800

    Update dynDNSCron.sh

--- dynDNSCron.sh
+++ dynDNSCron.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 echo "[INFO] [DynDNSCron] Regenerating ACL.."
-source /generateACL.sh
-echo "[INFO] [DynDNSCron] ACL regenerated!"
+
+# Apply timeout to the generateACL.sh script (5 seconds or adjust as needed)
+timeout 10s bash -c "source /generateACL.sh"
+retVal=$?
+if [ $retVal -eq 124 ]; then
+  echo "[ERROR] [DynDNSCron] generateACL.sh timed out!"
+elif [ $retVal -ne 0 ]; then
+  echo "[ERROR] [DynDNSCron] generateACL.sh failed with exit code $retVal!"
+else
+  echo "[INFO] [DynDNSCron] ACL regenerated!"
+fi
+
 echo "[INFO] [DynDNSCron] Reloading NginxDist ACl config"
 /usr/bin/dog @127.0.0.1:5300 --short reload.acl.miniproxy.local
 retVal=$?
@@ -10,7 +20,6 @@ if [ $retVal -eq 0 ]; then
 else
   echo "[ERROR] [DynDNSCron] Failed to reload NginxDist ACL config!"
 fi
-
 
 echo "[INFO] [DynDNSCron] reloading nginx..."
 /usr/sbin/nginx -s reload
