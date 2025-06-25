75644ad1b7c22a1700e78c74f1f082c4b71958ba
alexanderras84 <alexanderras@gmail.com>
2025-06-25 21:12:44 +0800

    Update dynDNSCron.sh

--- dynDNSCron.sh
+++ dynDNSCron.sh
@@ -12,15 +12,6 @@ else
   echo "[INFO] [DynDNSCron] ACL regenerated!"
 fi
 
-echo "[INFO] [DynDNSCron] Reloading NginxDist ACl config"
-/usr/bin/dog @127.0.0.1:5300 --short reload.acl.miniproxy.local
-retVal=$?
-if [ $retVal -eq 0 ]; then
-  echo "[INFO] [DynDNSCron] NginxDist ACL config successfully reloaded!"
-else
-  echo "[ERROR] [DynDNSCron] Failed to reload NginxDist ACL config!"
-fi
-
 echo "[INFO] [DynDNSCron] reloading nginx..."
 /usr/sbin/nginx -s reload
 echo "[INFO] [DynDNSCron] nginx successfully reloaded"
